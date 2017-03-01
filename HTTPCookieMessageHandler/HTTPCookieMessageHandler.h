34aa3013881e322636f785b4153307e2db1a43e8
Swindle, Gregory Jay (Greg) <gregory.jay.swindle@one.verizon.com>
2017-03-01 01:23:43 -0600

    Initial Commit

---
+++ HTTPCookieMessageHandler/HTTPCookieMessageHandler.h
@@ -0,0 +1,19 @@
+//
+//  HTTPCookieMessageHandler.h
+//  HTTPCookieMessageHandler
+//
+//  Created by Swindle, Gregory Jay (Greg) on 3/1/17.
+//  Copyright © 2017 Swindle, Gregory Jay (Greg). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTTPCookieMessageHandler.
+FOUNDATION_EXPORT double HTTPCookieMessageHandlerVersionNumber;
+
+//! Project version string for HTTPCookieMessageHandler.
+FOUNDATION_EXPORT const unsigned char HTTPCookieMessageHandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPCookieMessageHandler/PublicHeader.h>
+
+
